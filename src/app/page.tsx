15bd35481ac791e474118db40b51d08192fe44c6
ViniHGV <ixxvinicius@gmail.com>
2023-10-24 11:03:49 -0300

    Adicionando imagem do mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+import ImageMobile from "../../public/ImageMobile.png";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="relative">
+      <Image
+        alt=""
+        className="absolute w-full object-contain top-0"
+        src={ImageMobile}
+      />
+    </main>
+  );
 }
