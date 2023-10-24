6746cdda4b32be271c7cc203ddf6682938a5a823
ViniHGV <ixxvinicius@gmail.com>
2023-10-24 11:12:02 -0300

    Adicionando Fonte de titulos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Rufina } from "next/font/google";
+
+const rufina = Rufina({
+  weight: "700",
+  variable: "--rufina-font",
+  subsets: ["latin"],
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${rufina.className}`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,14 @@ import ImageMobile from "../../public/ImageMobile.png";
 export default function Home() {
   return (
     <main className="relative">
-      <Image
+      {/* <Image
         alt=""
-        className="absolute w-full object-contain top-0"
+        className="absolute w-full object-contain top-0 lg:hidden"
         src={ImageMobile}
-      />
+      /> */}
+      <p className="text-white text-5xl z-10 font-title">
+        Victor Venturini Brandão
+      </p>
     </main>
   );
 }
